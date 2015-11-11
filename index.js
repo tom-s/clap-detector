f0d9c9d2fd3f4593c7afc05e6be4dc3a5819bc39
Thomas Schell <thom.schell@gmail.com>
2015-11-11 17:54:59 +0100

    Solved issues

--- index.js
+++ index.js
@@ -29,21 +29,21 @@
 var _ = require('lodash');
 var Q = require('q');
 var exec = require('child_process').exec;
+var fs = require('fs');
 
 var clapDetector = (function() {
     /* DEFAULT CONFIG */
     var CONFIG = {
         AUDIO_SOURCE: 'hw:1,0',
-        DETECTION_PERCENTAGE_START : '5%',
-        DETECTION_PERCENTAGE_END: '5%',
+        DETECTION_PERCENTAGE_START : '10%',
+        DETECTION_PERCENTAGE_END: '10%',
         CLEANING: {
             PERFORM: false, // requires a noise profile
             NOISE_PROFILE: 'noise.prof'
         },
-        SOUND_FILE : "input.wav", // input file
-        SOUND_FILE_CLEAN  : "input-clean.wav",
         CLAP_AMPLITUDE_THRESHOLD: 0.7,
         CLAP_ENERGY_THRESHOLD: 0.3,
+        CLAP_MAX_DURATION: 1500,
         MAX_HISTORY_LENGTH: 10 // no need to maintain big history
     };
 
@@ -89,8 +89,11 @@ var clapDetector = (function() {
 
     /* Listen */
     function _listen() {
+        var random = _.random(0,99999);
+        var filename = 'input' + random + '.wav';
+
         // Listen for sound
-        var cmd = 'sox -t alsa ' + CONFIG.AUDIO_SOURCE + ' ' + CONFIG.SOUND_FILE + ' silence 1 0.0001 '  + CONFIG.DETECTION_PERCENTAGE_START + ' 1 0.1 ' + CONFIG.DETECTION_PERCENTAGE_END;
+        var cmd = 'sox -t alsa ' + CONFIG.AUDIO_SOURCE + ' ' + filename + ' silence 1 0.0001 '  + CONFIG.DETECTION_PERCENTAGE_START + ' 1 0.1 ' + CONFIG.DETECTION_PERCENTAGE_END;
         var child = exec(cmd);
         child.on('close', function() {
             if(paused) {
@@ -100,61 +103,69 @@ var clapDetector = (function() {
             _listen(); // listen again
 
             // Clean and check file and take action
-            _cleanFile().then(function() {
-                _isClap().then(function() {
-                    clapsHistory.push({
-                        time: new Date().getTime()
-                    });
-                    if(EVENTS.clap.fn) {
-                        EVENTS.clap.fn();
+            _cleanFile(filename).then(function(newFilename) {
+                _isClap(newFilename).then(function(isClap) {
+                    if(isClap) {
+                        clapsHistory.push({
+                            time: new Date().getTime()
+                        });
+                        if(EVENTS.clap.fn) {
+                            EVENTS.clap.fn();
+                        }
+                        _handleMultipleClaps();
                     }
-                    _handleMultipleClaps();
+                    // remove file
+                    fs.unlink(filename);
                 });
             });
 
         });
     }
 
-    function _isClap() {
+    function _isClap(filename) {
 
         var deferred = Q.defer();
 
-         // Check that file is a clap
-         var cmd = "sox " + CONFIG.SOUND_FILE_CLEAN + " -n stat 2>&1"; //| sed -n 's#^Length (seconds):[^0-9]*\\([0-9.]*\\)$#\\1#p'
-         var regExDuration = /Length[\s]+\(seconds\):[\s]+([0-9.]+)/;
-         var regExRms = /RMS[\s]+amplitude:[\s]+([0-9.]+)/;
-         var regExMax = /Maximum[\s]+amplitude:[\s]+([0-9.]+)/;
-         exec(cmd, function(error, out, stderr) {
-             // Is this a clap of hand ?
-             var durationData = out.match(regExDuration);
-             var duration = parseFloat(durationData[1]);
-             var rmsData = out.match(regExRms);
-             var rms = parseFloat(rmsData[1]);
-             var maxData = out.match(regExMax);
-             var max = parseFloat(maxData[1]);
-
-             // Does it have the characteristics of a clap
-             if(duration < 1 && max > CONFIG.CLAP_AMPLITUDE_THRESHOLD && rms < CONFIG.CLAP_ENERGY_THRESHOLD){
-                deferred.resolve(res);
-             } else {
+        // Check that file is a clap
+        var cmd = "sox " + filename + " -n stat 2>&1"; //| sed -n 's#^Length (seconds):[^0-9]*\\([0-9.]*\\)$#\\1#p'
+        var regExDuration = /Length[\s]+\(seconds\):[\s]+([0-9.]+)/;
+        var regExRms = /RMS[\s]+amplitude:[\s]+([0-9.]+)/;
+        var regExMax = /Maximum[\s]+amplitude:[\s]+([0-9.]+)/;
+        exec(cmd, function(error, out, stderr) {
+            if(error) {
                 deferred.resolve(false);
-             }
-         });
+            } else {
+                // Is this a clap of hand ?
+                var durationData = out.match(regExDuration);
+                var duration = parseFloat(durationData[1]);
+                var rmsData = out.match(regExRms);
+                var rms = parseFloat(rmsData[1]);
+                var maxData = out.match(regExMax);
+                var max = parseFloat(maxData[1]);
+
+                // Does it have the characteristics of a clap
+                if(duration < CONFIG.CLAP_MAX_DURATION/1000 && max > CONFIG.CLAP_AMPLITUDE_THRESHOLD && rms < CONFIG.CLAP_ENERGY_THRESHOLD){
+                    deferred.resolve(true);
+                } else {
+                    deferred.resolve(false);
+                }
+            }
+        });
 
         return deferred.promise;
     }
 
-    function  _cleanFile() {
+    function  _cleanFile(filename) {
         var deferred = Q.defer();
         if(CONFIG.CLEANING.PERFORM) {
             // Clean noise
-            var cmd = 'sox ' + CONFIG.SOUND_FILE + ' ' + CONFIG.SOUND_FILE_CLEAN + ' noisered ' + CONFIG.CLEANING.NOISE_PROFILE + ' 0.21';
+            var cmd = 'sox ' + filename + ' ' + 'clean-' + filename + ' noisered ' + CONFIG.CLEANING.NOISE_PROFILE + ' 0.21';
             exec(cmd, function() {
-                deferred.resolve();
+                deferred.resolve('clean-' + filename);
             });
         } else {
             // No cleaning to do
-            deferred.resolve();
+            deferred.resolve(filename);
         }
 
         return deferred.promise;
