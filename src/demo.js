390d7facf42442f18d6303e550d32f95d3d0e54d
Thomas Schell <thom.schell@gmail.com>
2018-12-29 19:03:29 +0100

    clean

--- src/demo.js
+++ src/demo.js
@@ -2,26 +2,26 @@
 import ClapDetector from './index'
 
 const clap = new ClapDetector()
-const oneClapListener = clap.addClapsListener(props => {
+const cancelOneClapListener = clap.addClapsListener(props => {
   console.log("debug 1 clap force", props)
 }, { number: 1, delay: 0, force: true })
 
-const oneClapForceListener = clap.addClapsListener(props => {
+const cancelOneClapForceListener = clap.addClapsListener(props => {
   console.log("debug 1 clap", props)
 }, { number: 1, delay: 1000 })
 
-const twoClapsListener = clap.addClapsListener(props => {
+const cancelTwoClapsListener = clap.addClapsListener(props => {
   console.log("debug 2 claps", props)
 }, { number: 2, delay: 1000 })
 
-const threeClapsListener = clap.addClapsListener(props => {
+const cancelThreeClapsListener = clap.addClapsListener(props => {
   console.log("debug 3 claps", props)
 }, { number: 3, delay: 1000 })
 
 // Example: canceling clap listeners
 // Cancel alls claps listener but 2 claps after 10 seconds
 setTimeout(() => {
-  oneClapListener()
-  oneClapForceListener()
-  threeClapsListener()
+  cancelOneClapListener()
+  cancelOneClapForceListener()
+  cancelThreeClapsListener()
 }, 10000)
